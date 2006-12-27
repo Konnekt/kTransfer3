c2813d1c8da01a5bbf8f7a26051ff2c78beec324
Sijawusz Pur Rahnama <sija@sija.pl>
2006-12-27 11:16:23 +0000

    git-svn-id: http://www.konnekt.info/svn/ktransfer3/trunk@20 c7aaf6ec-9b21-0410-9114-9fd2c749af1b

--- kTransfer3.cpp
+++ kTransfer3.cpp
@@ -1,22 +1,12 @@
-#ifdef _MANAGED
-#pragma managed(push, off)
-#endif
-
 #include "stdafx.h"
-#include <windows.h>
 #include "Controller.h"
 
 using namespace kTransfer3;
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-  return TRUE;
+  return true;
 }
 
 int __stdcall IMessageProc(sIMessage_base * msgBase) {
   return Controller::getInstance()->process(msgBase);
 }
-
-#ifdef _MANAGED
-#pragma managed(pop)
-#endif
-
